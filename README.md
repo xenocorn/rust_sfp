c87547328950411153a0cd3e6729e61f39c931e4
XenoCorn <>
2021-08-22 18:16:22 +0300

    v 0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rust_sfp
+Rust implementation of [Simple Frame Protocol](https://github.com/xenocorn/SFP/blob/master/README.md)
+over TCP and unix domain sockets
---
+++ src/lib.rs
@@ -0,0 +1,123 @@
+pub use unisocket::SocketAddr;
+use unisocket::Stream;
+use std::io;
+use std::time::Duration;
+use std::net::{TcpStream, Shutdown};
+#[cfg(unix)]
+use std::os::unix::net as unix;
+use std::io::{Read, Write};
+use std::fmt;
+use std::fmt::{Formatter, Debug};
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+#[derive(Debug)]
+pub enum WriteErr{
+    I0(io::Error),
+    TooLongFrame,
+}
+
+impl fmt::Display for WriteErr{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self{
+            WriteErr::I0(err) => { std::fmt::Display::fmt(&err, f) }
+            WriteErr::TooLongFrame => {
+                write!(f, "{}", "Frame is too long to send by SFP")
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Connection{
+    pub stream: Stream
+}
+
+impl From<Stream> for Connection{
+    fn from(stream: Stream) -> Self {
+        Self{stream}
+    }
+}
+
+impl From<TcpStream> for Connection{
+    fn from(s: TcpStream) -> Self {
+        Self::from(Stream::from(s))
+    }
+}
+
+#[cfg(unix)]
+impl From<unix::UnixStream> for Connection{
+    fn from(s: unix::UnixStream) -> Self {
+        Self::from(Stream::from(s))
+    }
+}
+
+impl Connection{
+    pub fn connect(s: &SocketAddr) -> io::Result<Self> {
+        match Stream::connect(s) {
+            Ok(stream) => {
+                Ok(Self::from(stream))
+            }
+            Err(err) => {
+                Err(err)
+            }
+        }
+    }
+    pub fn local_addr(&self) -> io::Result<SocketAddr> {
+        self.stream.local_addr()
+    }
+    pub fn peer_addr(&self) -> io::Result<SocketAddr> {
+        self.stream.peer_addr()
+    }
+    pub fn set_read_timeout(&self, t: Option<Duration>) -> io::Result<()> {
+        self.stream.set_read_timeout(t)
+    }
+    pub fn set_write_timeout(&self, t: Option<Duration>) -> io::Result<()> {
+        self.stream.set_write_timeout(t)
+    }
+    pub fn shutdown(&self, t: Shutdown) -> io::Result<()> {
+        self.stream.shutdown(t)
+    }
+    pub fn try_clone(&self) -> io::Result<Self>{
+        Ok(Self::from(self.stream.try_clone()?))
+    }
+    pub fn read_frame(&mut self) -> io::Result<Vec<u8>>{
+        let mut header = [0u8; 4];
+        self.stream.read_exact(&mut header)?;
+        let length = u32::from_be_bytes(header) as usize;
+        let mut frame = vec![0u8; length];
+        self.stream.read_exact(&mut *frame)?;
+        Ok(frame)
+    }
+    pub fn write_frame(&mut self, frame: &mut [u8]) -> Result<(), WriteErr>{
+        let length = frame.len();
+        if length > u32::MAX as usize {
+            return Err(WriteErr::TooLongFrame)
+        }
+        let mut header:[u8; 4] = u32::to_be_bytes(length as u32);
+        if let Err(err) = self.stream.write_all(&mut header){return Err(WriteErr::I0(err))}
+        if let Err(err) = self.stream.write_all(frame){return Err(WriteErr::I0(err))}
+        Ok(())
+    }
+    pub fn flush(&mut self) -> io::Result<()> {
+        self.stream.flush()
+    }
+}
+
+impl Iterator for Connection{
+    type Item = Vec<u8>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.read_frame(){
+            Ok(frame) => {Some(frame)}
+            Err(_) => {None}
+        }
+    }
+}
